c2bb44cba1203a8841fbb419947a36e031a7151a
Art Segura <art@ivymicro.cc>
2017-01-10 22:26:28 -0600

    NetBeans added spaces between each line in the code

--- public/logout.php
+++ public/logout.php
@@ -1,23 +1,23 @@
-<?php require_once("../includes/session.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
-
-<?php
-    // Version 1
-    // Set the user Session variables to null and redirect to the login page
-    //$_SESSION["admin_id"] = null;
-    //$_SESSION["username"] = null;
-    //redirect_to("login.php");
-    
-?>
-
-<?php
-    // Version 2
-    // This completely destroys the current Session
-    session_start();
-    $_SESSION = array();
-    if(isset($_COOKIE[session_name()])) {
-        setcookie(session_name(), '', time()-42000, '/');
-    }
-    session_destroy();
-    RedirectTo("login.php");
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+
+<?php
+    // Version 1
+    // Set the user Session variables to null and redirect to the login page
+    //$_SESSION["admin_id"] = null;
+    //$_SESSION["username"] = null;
+    //redirect_to("login.php");
+    
+?>
+
+<?php
+    // Version 2
+    // This completely destroys the current Session
+    session_start();
+    $_SESSION = array();
+    if(isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time()-42000, '/');
+    }
+    session_destroy();
+    RedirectTo("login.php");
 ?>
